641771982b2553df29678858e21e7fef0afcb5e6
Aryan Mishra <mishra.aryann100@gmail.com>
2025-01-06 19:18:02 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-loc = "/home/ryan/IS/WOC/2strings"
+loc = "/home/ryan/IS/WOC/7str"
 words = "/home/ryan/IS/WOC/words1"
 ex = "/home/ryan/IS/WOC/wordsextracted"
 string = ""
@@ -11,15 +11,22 @@ a = input()
 if(a == "Y" or a == "y" or a == "yes" or a == "Yes" or a == "YES"):
   words = "/home/ryan/IS/WOC/words2"
 
+with open(words) as f:
+  wor = f.read()
+with open(loc) as file:
+  string = file.read()
+
+lines1 = wor.split("\n")
+lines2 = string.split("\n")
+
 with open(ex, "w") as fi:
-  with open(loc) as file:
-    for string in file:
-      with open(words) as f:
-        for wor in f:
-          i = string.lower().find(wor.lower())
-          b = len(wor)
-          if(i != -1):
-            fi.write(string[i:i+b])
+  for line1 in lines1:
+    for line2 in lines2:
+      i = line2.lower().find(line1.lower())
+      b = len(line1)
+      if(i != -1):
+        fi.write(line2[i:i+b])
+        fi.write("\n")
             
 print("Extracted Words -")
 
